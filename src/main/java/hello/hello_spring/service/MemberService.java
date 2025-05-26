4e4fa311e96c2b0fd4018a159876c3b9e700c0be
limgyounggeun <choho4843@gmail.com>
2025-05-26 23:56:14 +0900

    자바 ㅋ도ㅡë¡자바 코드로 직접 스프링 빈 등록하기

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -3,6 +3,7 @@ package hello.hello_spring.service;
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.Optional;
 
 public class MemberService {
     private final MemberRepository repository;
+
 
     public MemberService(MemberRepository repository) {
         this.repository = repository;
